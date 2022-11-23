e5715cc165c57de49a3aad879547132b091636cd
M Starch <LeStarch@googlemail.com>
2022-11-22 18:31:43 -0800

    recursion fixes

--- bin/template-maker.py
+++ bin/template-maker.py
@@ -1,33 +1,55 @@
+""" template-maker.py: Builds templates found within a set of FPP files
 
+Makes templates from FPP files. This is performed both on FPPs, included FPPs, and recursively through generated
+templates. Templates are generated from stylized includes of the form:
+
+```
+include "<template base name>.<template instance name>.fppt"
+```
+Template inputs are of the form `<template base name>.fppt` found in any listed fprime library.
+
+
+@copyright
+Copyright 2009-2015, by the California Institute of Technology.
+ALL RIGHTS RESERVED.  United States Government Sponsorship
+acknowledged.
+
+@author mstarch
+"""
 import argparse
 import itertools
+import os
+import shutil
 import sys
 import re
 
 from pathlib import Path
-from typing import List, Tuple, Union
+from typing import Dict, List, Tuple, Union
 
 import jinja2
 
 
 TEMPLATE_FOLDER_NAME = "topology-templates"
-TEMPLATE_SUFFIX = "fppt"
+SNIPPETS_FOLDER_NAME = "snippets"
+TEMPLATE_SUFFIX = ".fppt"
 
 
-def template_information(template_invocation_path: Path, fprime_locations: Union[List[str], None] = None) -> Tuple[Path, str, str]:
+def template_information(template_invocation_path: Path, fprime_locations: List[str]) -> Tuple[Path, str, str]:
     """ Calculate path to template path, template base name, and template name based on template invocation """
-    template_definition = f"{ Path(template_invocation_path.stem).stem }.{ template_invocation_path.suffix }"
+    template_definition = f"{ Path(template_invocation_path.stem).stem }{ template_invocation_path.suffix }"
+    template_name = Path(template_invocation_path.stem).suffix.lstrip(".")
+
     possible_locations = [Path(possible) / TEMPLATE_FOLDER_NAME / template_definition for possible in fprime_locations]
     template_locations = [possible for possible in possible_locations if possible.exists() and possible.is_file()]
 
-    if fprime_locations is not None and not template_locations:
+    if not template_locations:
         string_locations = ','.join([str(location) for location in possible_locations])
         raise FileNotFoundError(f"No template found in any of: { string_locations }")
 
-    if fprime_locations is not None and len(template_locations) > 1:
+    if len(template_locations) > 1:
         string_locations = ','.join([str(location) for location in template_locations])
         raise FileNotFoundError(f"Multiple template definitions: { string_locations }")
-    return None if fprime_locations is None else template_locations[0], template_definition, Path(template_invocation_path.stem).suffix
+    return template_locations[0], template_definition, template_name
 
 
 def setup_environment(fprime_locations: List[str]) -> jinja2.Environment:
@@ -50,46 +72,80 @@ def get_template_invocations(path: Path, fprime_locations: List[str], pat: re.Pa
     """ Calculate template invocations contained within supplied file """
 
     # Find all include matches in this file
-    with open(path) as file_handle:
+    with open(path, "r") as file_handle:
         matches = [pat.match(line) for line in file_handle]
     matches = [match.group(1) for match in matches if match]
 
     # Determine this file's invocations and this file's normal includes
-    match_file_suffix = f".{ TEMPLATE_SUFFIX }"
-    invocations = [path / match for match in matches if match.endswith(match_file_suffix)]
-    includes = [path / match for match in matches if not match.endswith(match_file_suffix)]
+    invocations = [path.parent / match for match in matches if match.endswith(TEMPLATE_SUFFIX)]
+    includes = [path.parent / match for match in matches if not match.endswith(TEMPLATE_SUFFIX)]
 
-    # Calculate recursive invocations by looking at the base template for invocations and include file for includes
-    template_infos = [template_information(invocation, fprime_locations) for invocation in invocations]
-    base_templates = [template for template, _, _ in template_infos]
-    recursive_invocations = [get_template_invocations(path, fprime_locations) for path in base_templates + includes]
+    # Calculate invocations hidden in includes
+    recursive_invocations = [get_template_invocations(path, fprime_locations) for path in includes]
 
     # Return local invocations and everything found recursively
-    return invocations + list(itertools.chain(*recursive_invocations))
+    return invocations + list(itertools.chain.from_iterable(recursive_invocations))
 
 
-def build_templates(template_invocations: List[Path], environment: jinja2.Environment, template_offset_multiple: int):
-    """ Builds needed templates from list of templates """
+def manufacture_files(data: str, output_file: Path, base_template: Path) -> List[Path]:
+    """ Write out necessary files for template """
+    # Make sure the parent path exists
+    output_file.parent.mkdir(parents=True, exist_ok=True)
+    snippets_output = output_file.parent / SNIPPETS_FOLDER_NAME
+    snippets_output.mkdir(parents=True, exist_ok=True)
+    snippets_input = base_template.parent / SNIPPETS_FOLDER_NAME
+
+    # Copy snippets to output snippets folder
+    snippets = []
+    for current, _, files in os.walk(snippets_input):
+        input_directory = Path(current)
+        output_directory = snippets_output / input_directory.relative_to(snippets_input)
+
+        output_directory.mkdir(parents=True, exist_ok=True)
+        for file in files:
+            shutil.copy(input_directory / file, output_directory / file)
+            snippets.append(input_directory / file)
+
+    # Write template invocation file
+    with open(output_file, "w") as file_handle:
+        file_handle.write(data)
+    return snippets
+
+
+def build_templates(path: Path, locations: List[str], environment: jinja2.Environment, step: int, offset: int = 0, counts: Union[Dict[str, int], None] = None) -> List[Path]:
+    """ Builds needed templates from input file """
     base_parameters = {}  # TODO: fill with configurable parameters
-    instance_counts = {}
-    template_offset = 0
+    counts = {} if counts is None else counts
 
+    template_invocations = get_template_invocations(path, locations)
+
+    local_template_files = []
+    invocations = []
+    snippets = []
     for invocation in template_invocations:
-        _, template_definition, template_name = template_information(invocation)
+        template_path, template_definition, template_name = template_information(invocation, locations)
         template = environment.get_template(template_definition)
 
-        instance_counts[template_definition] = instance_counts.get(template_definition, -1) + 1
+        counts[template_definition] = counts.get(template_definition, -1) + 1
         template_parameters = {
             "template_name": template_name,
-            "template_index": instance_counts[template_definition],
-            "template_offset": template_offset,
+            "template_index": counts[template_definition],
+            "template_offset": offset,
             **base_parameters
         }
-        template_offset += template_offset_multiple
+        offset += step
 
         rendered = template.render(**template_parameters)
-        with open(invocation, "w") as file_handle:
-            file_handle.write(rendered)
+        snippets.extend(manufacture_files(rendered, invocation, template_path))
+
+        local_template_files.append(template_path)
+        invocations.append(invocation)
+
+    recursive_template_files = [
+        build_templates(invocation, locations, environment, step, offset, counts)
+        for invocation in invocations
+    ]
+    return local_template_files + snippets + list(itertools.chain.from_iterable(recursive_template_files))
 
 
 def main(args: List[str]):
@@ -97,19 +153,25 @@ def main(args: List[str]):
     parser = argparse.ArgumentParser(description="Template processing script")
     parser.add_argument("--fprime-locations", nargs="+", help="Locations of fprime packages/libraries")
     parser.add_argument("--topology-files", type=Path, nargs="+", help="Paths the the main topology files")
-    parser.add_argument("--offset-multiple", type=int, help="Offset multiple for each templated topology hunk")
+    parser.add_argument("--offset-multiple", type=lambda x: int(x, 0),
+                        help="Offset multiple for each templated topology hunk")
     args_ns = parser.parse_args(args)
-
     try:
-        environment = setup_environment(args_ns.fprime_locations)
-        file_invocations = [get_template_invocations(path, args_ns.fprime_locations) for path in args_ns.topology_files]
-        invocations = list(itertools.chain(*file_invocations))
-        build_templates(invocations, environment, args_ns.offset_multiple)
+        # Mine out the passed in settings
+        locations = args_ns.fprime_locations
+        inputs = args_ns.topology_files
+        offset = args_ns.offset_multiple
+
+        # Build jinja templates
+        environment = setup_environment(locations)
+        template_sets = [build_templates(path, locations, environment, offset) for path in inputs]
+        [print(template_file) for template_file in set(itertools.chain.from_iterable(template_sets))]
     except Exception as exc:
         print(f"[ERROR] {exc}", file=sys.stderr)
+        raise
         return 1
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv[1:]))
--- cmake/templates.cmake
+++ cmake/templates.cmake
@@ -1,11 +1,45 @@
+####
+# templates.cmake:
+#
+# Topology template support.  Calls `template-maker.py` when invoked in `make_templates(...)`. This must happen before
+# any calls to `register_fprime_...` functions
+#
+# Copyright 2009-2015, by the California Institute of Technology.
+# ALL RIGHTS RESERVED.  United States Government Sponsorship
+# acknowledged.
+####
+set(TEMPLATES_EXECUTABLE_PATH "${CMAKE_CURRENT_LIST_DIR}/../bin/template-maker.py")
 
-set(TEMPLATES_EXECUTABLE_PATH "${CMAKE_CURRENT_LIST_DIR}/../bin/templates-maker.py")
-
-function(make_templates OFFSET_MULTIPLE TEMPLATE_SOURCE_FILE)
-    execute_process(
-        COMMAND ${PYTHON} ${TEMPLATES_EXECUTABLE_PATH}
-            "--fprime-locations" ${FPRIME_BUILD_LOCATIONS}
-            "--topology-files" ${TEMPLATE_SOURCE_FILE}
-            "--offset-multiple" "${OFFSET_MULTIPLE}"
+####
+# make_templates: make templates inline
+#
+# - TEMPLATE_SOURCE_FILES: list of FPP input files
+# - OFFSET_MULTIPLE: multiple of base id steps in templates
+#####
+function(make_templates TEMPLATE_SOURCE_FILES OFFSET_MULTIPLE)
+    set(EXECUTE_ARGUMENTS
+        ${PYTHON} ${TEMPLATES_EXECUTABLE_PATH}
+        "--fprime-locations" ${FPRIME_BUILD_LOCATIONS}
+        "--topology-files" ${TEMPLATE_SOURCE_FILES}
+        "--offset-multiple" "${OFFSET_MULTIPLE}"
     )
+    # Print out output
+    if (CMAKE_DEBUG_OUTPUT)
+        string(REPLACE ";" " " EXECUTE_STRING "${EXECUTE_ARGUMENTS}")
+        message(STATUS "Template Generation: ${EXECUTE_STRING}")
+    endif()
+    # Execute the process
+    execute_process(COMMAND ${EXECUTE_ARGUMENTS}
+        OUTPUT_VARIABLE FILE_LISTING
+        RESULT_VARIABLE RESULT_CODE
+    )
+    # Check result
+    if (NOT RESULT_CODE EQUAL 0)
+        message(FATAL_ERROR "Failed to run template generation.")
+    endif()
+    # Mark files as templates
+    string(REPLACE "\n" ";" FILE_LISTING "${FILE_LISTING}")
+    foreach(FILE_ITEM IN LISTS FILE_LISTING)
+        set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${FILE_ITEM}")
+    endforeach()
 endfunction(make_templates)
